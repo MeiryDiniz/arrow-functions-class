9d68d3efb3b029b5326769ec600de0ffccbf1337
MeiryDiniz <meirydiniz@icloud.com>
2024-04-06 17:47:47 +0000

    Add function

---
+++ node/arrowfunctions.js
@@ -0,0 +1,23 @@
+/**
+ * To run this file, use the command: node arrow-functions.js
+ * in the terminal
+ */
+
+// Vanilla JavaScript Function
+
+function addTwoNumbers(a, b) {
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
+
+
+
+// Arrow Function With Parameters
+
+// Single Line Arrow Function With Parameters
+
+// Implicit Returns
+
+// Returning Multiple Lines
